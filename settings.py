2b37a76e546667141851e8096e589341c59f31bf
Dmytro Pershyn <dpershyn@hotmail.com>
2015-06-06 19:38:45 +0300

    trading project

---
+++ settings.py
@@ -0,0 +1,18 @@
+ENVIRONMENTS = {
+    "streaming": {
+        "real": "stream-fxtrade.oanda.com",
+        "practice": "stream-fxpractice.oanda.com",
+        "sandbox": "stream-sandbox.oanda.com"
+    },
+    "api": {
+        "real": "api-fxtrade.oanda.com",
+        "practice": "api-fxpractice.oanda.com",
+        "sandbox": "api-sandbox.oanda.com"
+    }
+}
+
+DOMAIN = "practice"
+STREAM_DOMAIN = ENVIRONMENTS["streaming"][DOMAIN]
+API_DOMAIN = ENVIRONMENTS["api"][DOMAIN]
+ACCESS_TOKEN = 'bd69cd6ecf7153200187480a9339ff3d-ddc9d31c4533e18cee4ba6e00e835ef4'
+ACCOUNT_ID = '9084727'
